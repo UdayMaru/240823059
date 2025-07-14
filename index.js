fd31e1b6f9b9de9b953158229643dd2756c2283a
Uday Maru <maruuday3@gmail.com>
2025-07-14 11:40:45 +0530

    2 program

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ console.log(name)
 
 const surname = "maru"
 console.log(surname)
+
+
+
+//1 check even or odd
+
+let number = 7;
+
+if (number % 2 === 0) {
+    console.log("Even");
+}else {
+    console.log("odd")
+}
+
+
+//2 find the maximum of two numbers
+
